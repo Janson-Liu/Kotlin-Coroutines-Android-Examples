ef18c3dff6a3912498779d8b0a405e7398b388fb
janson__liu <janson.liu@twobulls.com>
2021-12-13 15:41:55 +1030

    test Bitrise

--- app/src/main/java/com/mindorks/example/coroutines/data/api/ApiHelperImpl.kt
+++ app/src/main/java/com/mindorks/example/coroutines/data/api/ApiHelperImpl.kt
@@ -1,5 +1,7 @@
 package com.mindorks.example.coroutines.data.api
 
+import com.mindorks.example.coroutines.data.model.ApiUser
+
 class ApiHelperImpl(private val apiService: ApiService) : ApiHelper {
 
     override suspend fun getUsers() = apiService.getUsers()
@@ -7,5 +9,8 @@ class ApiHelperImpl(private val apiService: ApiService) : ApiHelper {
     override suspend fun getMoreUsers() = apiService.getMoreUsers()
 
     override suspend fun getUsersWithError() = apiService.getUsersWithError()
+    override suspend fun deleteUser(): List<ApiUser> {
+        return listOf()
+    }
 
 }
